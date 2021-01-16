f663f59b2f9dc0692df6c1fbc24810f72805df10
Javier Lopez <javisan81@hotmail.com>
2021-01-16 10:51:15 +0100

    first test created

---
+++ src/test/java/PrimeNumbersTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrimeNumbersTest {
+    private List<Integer> primeNumbers(int number) {
+    }
+
+    @Test
+    public void severalPrimeNumbers(){
+        assertEquals(emptyList(), primeNumbers(1));
+    }
+}
