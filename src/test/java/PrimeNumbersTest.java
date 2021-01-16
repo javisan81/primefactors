91ee36e2de092887e474bca3ef0917541273b0d3
Javier Lopez <javisan81@hotmail.com>
2021-01-16 10:59:45 +0100

    two transformations (unconditional->if), (nil->constant)

--- src/test/java/PrimeNumbersTest.java
+++ src/test/java/PrimeNumbersTest.java
@@ -3,15 +3,19 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrimeNumbersTest {
     private List<Integer> primeNumbers(int number) {
-        return emptyList();
+        if (number == 1)
+            return emptyList();
+        return singletonList(2);
     }
 
     @Test
-    public void severalPrimeNumbers(){
+    public void severalPrimeNumbers() {
         assertEquals(emptyList(), primeNumbers(1));
+        assertEquals(singletonList(2), primeNumbers(2));
     }
 }
