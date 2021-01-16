63eaeedec6fe2dcadc049b59dbb9769c8e5c9539
Javier Lopez <javisan81@hotmail.com>
2021-01-16 11:06:04 +0100

    (unconditional->if) splitting the execution path

--- src/test/java/PrimeNumbersTest.java
+++ src/test/java/PrimeNumbersTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,6 +11,8 @@ public class PrimeNumbersTest {
     private List<Integer> primeNumbers(int number) {
         if (number == 1)
             return emptyList();
+        if (number == 4)
+            return asList(2,2);
         return singletonList(number);
     }
 
@@ -18,5 +21,6 @@ public class PrimeNumbersTest {
         assertEquals(emptyList(), primeNumbers(1));
         assertEquals(singletonList(2), primeNumbers(2));
         assertEquals(singletonList(3), primeNumbers(3));
+        assertEquals(asList(2, 2), primeNumbers(4));
     }
 }
